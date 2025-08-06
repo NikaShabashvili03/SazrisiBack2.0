69dedc131fc6268eab6122fdd8bbd662e4500cc7
NikaShabashvili <104011629+NikaShabashvili03@users.noreply.github.com>
2025-08-07 02:05:45 +0400

    pref

--- authentication/urls/user.py
+++ authentication/urls/user.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="user-register"),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('logout/', UserLogoutView.as_view(), name="user-logout"),
-    path('avatar', AvatarView.as_view(), name='avatar'),
-    path('preferences', PreferencesView.as_view(), name='preferences'),
+    path('avatar/', AvatarView.as_view(), name='avatar'),
+    path('preferences/', PreferencesView.as_view(), name='preferences'),
     path('change-password/', UserChangePassword.as_view(), name="change-password")
 ]
