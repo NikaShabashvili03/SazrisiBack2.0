a1474126c34bf4c29f454f67d1e53753d956445f
unknown <NikaShabashvili03>
2025-12-29 02:04:04 +0400

    new commit

--- authentication/views/user.py
+++ authentication/views/user.py
@@ -105,10 +105,7 @@ class UserLoginView(generics.GenericAPIView):
         
         user_data = UserProfileSerializer(user).data
         
-        response = Response({
-            "user": user_data,
-            "token": session.session_token
-        }, status=status.HTTP_201_CREATED)
+        response = Response(user_data, status=status.HTTP_200_OK)
 
         response.set_cookie(
             'session_token',
